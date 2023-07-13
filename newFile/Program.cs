a9fff9c82f387331e2a57c6e24fc69c0b51a80f6
Daniil <mr.varfolomeev@bk.ru>
2023-07-14 02:44:34 +0300

    completed 1 task

--- newFile/Program.cs
+++ newFile/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1. Написать программу, которая определяет, является ли треугольник со сторонами a, b, c равнобедренным.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+ 
+int number = Prompt("Введите трехзначное число > ");
+if (number < 100 || number >=1000)
+{
+    Console.WriteLine("Вы ввели не трехзначный код, пожалуйста повторите ввод");
+    return;
+}
+Console.WriteLine($"Введенное число '{number}'");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра'{secondRank}'");
